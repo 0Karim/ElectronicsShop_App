13d89ac8b23e17dd232033982f7f33b14d5551c7
Karim_Alaa <karim.alaa@dbsmena>
2021-01-01 17:40:34 +0200

    Add Product and Ctaegory Entity

---
+++ ElectronicShop_App/ElectronicShop.Domain/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ElectronicShop.Domain.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [ForeignKey("Category")]
+        public int? CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+    }
+}
